56085a3eb3ba9f3d8d412e246afba451fa124614
jaehyungpark <superjae0625@gmail.com>
2020-11-09 01:22:58 -0700

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
+
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -20,9 +22,17 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove('open');
     //null이 아닐 때만 출력(id가 클릭이 될 때만 출력/link가 있는경우만 출력)
     scrollIntoView(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
 
 //Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact')
