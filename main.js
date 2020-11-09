548091275112273278f86a56fe69c287d0729f0f
jaehyungpark <superjae0625@gmail.com>
2020-11-08 21:45:18 -0700

    JS: project filtering

--- main.js
+++ main.js
@@ -54,6 +54,28 @@ arrowUp.addEventListener('click', ()=> {
 });
 
 
+//Porjects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+    projectContainer.classList.add('anime-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter ==='*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anime-out');
+    }, 300);
+});
 
 
 
